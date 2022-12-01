30f24a37e7dd6177bdb140a384b271921d8a0554
user <you@example.com>
2022-12-01 10:02:14 +0300

    Optimized Task2

--- src/basic_tasks/Task2.java
+++ src/basic_tasks/Task2.java
@@ -8,24 +8,20 @@ public class Task2 {
     public static void findMinMaxAverage(int lengthOfArray) {
         int[] array = UtilClass.createRandomArray(lengthOfArray);
         int maxValue = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > maxValue) {
-                maxValue = array[i];
-            }
-        }
         int minValue = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < minValue) {
-                minValue = array[i];
-            }
-        }
         double sum = 0;
-        for (int i : array) {
-            sum += i;
+        for (int j : array) {
+            if (j > maxValue) {
+                maxValue = j;
+            }
+            if (j < minValue) {
+                minValue = j;
+            }
+            sum += j;
         }
         double average = sum / array.length;
-        System.out.println("Max-value : " + maxValue);
-        System.out.println("Min-value : " + minValue);
-        System.out.println("Average-value : " + average);
+        System.out.printf("Max-value : " + maxValue + "\n" +
+                "Min-value : " + minValue + "\n" +
+                "Average-value : " + average);
     }
 }
