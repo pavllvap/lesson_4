1f5095d53ac03bb1a7d28b76a88bc69aa35a5315
user <you@example.com>
2022-12-01 10:16:19 +0300

    Optimized Task6

--- src/aditional_tasks/Task6.java
+++ src/aditional_tasks/Task6.java
@@ -8,12 +8,7 @@ public class Task6 {
     }
 
     public static void createStringArray() {
-        String[] array = new String[5];
-        array[0] = "Vasya";
-        array[1] = "Kostya";
-        array[2] = "Sonya";
-        array[3] = "Sveta";
-        array[4] = "Kolya";
+        String[] array = new String[]{"Vasya", "Kostya", "Sonya", "Sveta", "Kolya"};
         System.out.println(Arrays.toString(array));
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
